1edf7bd682cd352bcaa2b36cb9e1a0a33f24c477
sakar jain <Jainsakar97@gmail.com>
2017-11-11 12:04:27 +0530

    setting up port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express=require("express");
 var socket=require("socket.io");
-
+const port=process.env.PORT||4000;
 var app=express();
-var server = app.listen(4000,function(){
+var server = app.listen(port,function(){
   console.log("listening to port 4000");
 });
 var io=socket(server);
