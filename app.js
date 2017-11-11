bfdee3beb2456a0450a3171d413db4d2af759eb6
sakar jain <Jainsakar97@gmail.com>
2017-11-11 10:59:27 +0530

    improved ui

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ var socket=require("socket.io");
 
 var app=express();
 var server = app.listen(4000,function(){
-  console.log("listening to port 3000");
+  console.log("listening to port 4000");
 });
 var io=socket(server);
 users=[];
 app.use(express.static("public"));
+//rendering index.html
 app.get("/",function(req,res){
   res.sendFile(__dirname+"/index.html");
 });
-io.on("connection",function(socket){
+io.on("connection",function(socket){ //checking for connection
   console.log("made socket connection");
   socket.on("chat",function(data){
     io.sockets.emit("chat",data);
@@ -19,6 +20,8 @@ io.on("connection",function(socket){
   socket.on("typing",function(data){
     socket.broadcast.emit("typing",data);
   });
+
+  //1st step checking valid user
   socket.on("check user",function(data,callback){
     if(users.indexOf(data)!=-1){
 
@@ -28,6 +31,7 @@ io.on("connection",function(socket){
       socket.nickname=data;
       users.push(data);
       io.sockets.emit("check user",users);
+      // io.sockets.emit("check user",data);
     }
   });
   socket.on("disconnect",function(){
